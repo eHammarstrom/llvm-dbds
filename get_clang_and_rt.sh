7cf4163e0cbdcf7623c748806dc720ef240a9057
Emil Hammarstrom <emil.a.hammarstrom@gmail.com>
2018-11-26 17:29:14 +0100

    clang-extras

--- get_clang_and_rt.sh
+++ get_clang_and_rt.sh
@@ -2,4 +2,8 @@
 
 git clone --single-branch -b release_70 https://github.com/llvm-mirror/clang tools/clang
 
+git clone --single-branch -b release_70 https://github.com/llvm-mirror/clang-tools-extra tools/extra
+
 git clone --single-branch -b release_70 https://github.com/llvm-mirror/compiler-rt projects/compiler-rt
+
+
