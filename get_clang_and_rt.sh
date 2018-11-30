e62050e4edb6ad278884993cd3034540131b073d
Emil Hammarstrom <emil.a.hammarstrom@gmail.com>
2018-11-30 19:36:13 +0100

    pull test-suite

--- get_clang_and_rt.sh
+++ get_clang_and_rt.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-git clone --single-branch -b release_70 https://github.com/llvm-mirror/clang tools/clang
+# git clone --single-branch -b release_70 https://github.com/llvm-mirror/clang tools/clang
 
-git clone --single-branch -b release_70 https://github.com/llvm-mirror/clang-tools-extra tools/clang/tools/extra
+# git clone --single-branch -b release_70 https://github.com/llvm-mirror/clang-tools-extra tools/clang/tools/extra
 
-git clone --single-branch -b release_70 https://github.com/llvm-mirror/compiler-rt projects/compiler-rt
+# git clone --single-branch -b release_70 https://github.com/llvm-mirror/compiler-rt projects/compiler-rt
 
+git clone --single-branch -b release_70 https://github.com/llvm-mirror/test-suite projects/test-suite
 
